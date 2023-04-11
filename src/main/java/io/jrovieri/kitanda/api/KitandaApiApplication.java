599f9652ab76d10eaada9f66e482a85d68338ecc
Jonathan Rovieri <jrovieri@gmail.com>
2023-04-11 00:52:49 -0300

    Spring template initial commit

---
+++ src/main/java/io/jrovieri/kitanda/api/KitandaApiApplication.java
@@ -0,0 +1,13 @@
+package io.jrovieri.kitanda.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitandaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitandaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jrovieri/kitanda/api/KitandaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jrovieri.kitanda.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitandaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
